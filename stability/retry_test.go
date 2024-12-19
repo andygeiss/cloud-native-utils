96086328cc07f6ddac8285bf0715adeab7fd4c06
Andy <4ndygeiss@gmail.com>
2024-12-19 14:21:55 +0100

    fix: remove typo

--- stability/retry_test.go
+++ stability/retry_test.go
@@ -16,7 +16,7 @@ func TestRetry_Succeeds(t *testing.T) {
 	assert.That(t, "result must be correct", res, 42)
 }
 
-func TestRetry_Suceeds_With_Retries(t *testing.T) {
+func TestRetry_Succeeds_With_Retries(t *testing.T) {
 	fn := stability.Retry[int](mockFailsTimes(2), 3, 10*time.Millisecond)
 	res, err := fn(context.Background(), 42)
 	assert.That(t, "err must be nil", err == nil, true)
