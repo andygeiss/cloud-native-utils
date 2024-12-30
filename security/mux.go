a31608d078ab3090e8844a9db037e3f1dc5b0c6f
Andy <4ndygeiss@gmail.com>
2024-12-30 11:57:42 +0100

    feat: add liveness and readiness probes #8

--- security/mux.go
+++ security/mux.go
@@ -1,16 +1,32 @@
 package security
 
 import (
+	"context"
 	"net/http"
 )
 
-// Mux returns a new http.ServeMux with the default health check endpoint.
-func Mux() *http.ServeMux {
+// Mux creates a new mux with the liveness check endpoint (/health)
+// and the readiness check endpoint (/ready).
+func Mux(ctx context.Context) *http.ServeMux {
+	// Create a new mux with liveness and readyness endpoint.
 	mux := http.NewServeMux()
-	// Health check endpoint for monitoring.
+
+	// Add a liveness check endpoint to the mux.
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
+		// Write OK to the response body.
 		w.Write([]byte("OK"))
 	})
+
+	// Add a readiness check endpoint to the mux.
+	mux.HandleFunc("/ready", func(w http.ResponseWriter, r *http.Request) {
+		select {
+		case <-ctx.Done():
+			w.WriteHeader(http.StatusServiceUnavailable)
+		default:
+			w.WriteHeader(http.StatusOK)
+		}
+	})
+
 	return mux
 }
--- security/mux_test.go
+++ security/mux_test.go
@@ -1,6 +1,7 @@
 package security_test
 
 import (
+	"context"
 	"net/http/httptest"
 	"testing"
 
@@ -9,15 +10,46 @@ import (
 )
 
 func TestServeMux_Is_Not_Nil(t *testing.T) {
-	mux := security.Mux()
-	assert.That(t, "mux must be not nil", mux != nil, true)
+	ctx := context.Background()
+	mux := security.Mux(ctx)
+	assert.That(t, "mux must not be nil", mux != nil, true)
 }
 
 func TestServeMux_Has_Health_Check(t *testing.T) {
-	mux := security.Mux()
+	ctx := context.Background()
+	mux := security.Mux(ctx)
 	req := httptest.NewRequest("GET", "/health", nil)
 	w := httptest.NewRecorder()
 	mux.ServeHTTP(w, req)
 	assert.That(t, "status code must be 200", w.Code, 200)
 	assert.That(t, "body must be OK", w.Body.String(), "OK")
+}
+
+func TestServeMux_Has_Readiness_Check_When_Context_Active(t *testing.T) {
+	ctx := context.Background()
+	mux := security.Mux(ctx)
+	req := httptest.NewRequest("GET", "/ready", nil)
+	w := httptest.NewRecorder()
+	mux.ServeHTTP(w, req)
+	assert.That(t, "status code must be 200", w.Code, 200)
+	// The body is empty in this example, but you can also check it if needed.
+}
+
+func TestServeMux_Has_Readiness_Check_When_Context_Canceled(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel() // Immediately cancel the context.
+	mux := security.Mux(ctx)
+	req := httptest.NewRequest("GET", "/ready", nil)
+	w := httptest.NewRecorder()
+	mux.ServeHTTP(w, req)
+	assert.That(t, "status code must be 503", w.Code, 503)
+}
+
+func TestServeMux_Unknown_Route(t *testing.T) {
+	ctx := context.Background()
+	mux := security.Mux(ctx)
+	req := httptest.NewRequest("GET", "/unknown", nil)
+	w := httptest.NewRecorder()
+	mux.ServeHTTP(w, req)
+	assert.That(t, "status code must be 404", w.Code, 404)
 }
