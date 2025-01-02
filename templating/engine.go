6ba1430f2b71346b2dd6387142be4dd8fef96744
Andy <4ndygeiss@gmail.com>
2025-01-02 14:39:35 +0100

    feat: add embed fs

--- templating/engine.go
+++ templating/engine.go
@@ -1,6 +1,7 @@
 package templating
 
 import (
+	"embed"
 	"fmt"
 	"io"
 	"text/template"
@@ -8,17 +9,20 @@ import (
 
 // Engine is a simple wrapper around the Go templating engine.
 type Engine struct {
+	efs  embed.FS
 	tmpl *template.Template
 }
 
 // NewEngine creates a new templating engine.
-func NewEngine() *Engine {
-	return &Engine{}
+func NewEngine(efs embed.FS) *Engine {
+	return &Engine{
+		efs: efs,
+	}
 }
 
 // Parse parses the templates in the given path.
-func (a *Engine) Parse(path string) {
-	tmpl, err := template.ParseGlob(path)
+func (a *Engine) Parse(patterns ...string) {
+	tmpl, err := template.ParseFS(a.efs, patterns...)
 	if err != nil {
 		panic(fmt.Sprintf("templating: could not parse templates: %v", err))
 	}
