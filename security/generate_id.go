6fbca61d44ec255f63469f62a16604c72271ac4a
andygeiss <4ndygeiss@gmail.com>
2025-09-17 16:04:31 +0200

    refactor: simplify return

--- security/generate_id.go
+++ security/generate_id.go
@@ -2,11 +2,10 @@ package security
 
 import (
 	"encoding/hex"
-	"fmt"
 )
 
 // GenerateID generates a unique ID using a secure random key.
 func GenerateID() string {
 	key := GenerateKey()
-	return fmt.Sprintf("%s", hex.EncodeToString(key[:]))
+	return hex.EncodeToString(key[:])
 }
