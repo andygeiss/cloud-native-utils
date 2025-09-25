d49506478efd4d4574f9b30fa476aee53482b322
andygeiss <4ndygeiss@gmail.com>
2025-09-25 14:34:57 +0200

    fix: encode code verifier

--- security/generate_pkce.go
+++ security/generate_pkce.go
@@ -7,7 +7,8 @@ import (
 
 // GeneratePKCE generates a OAuth 2.0 PKCE challenge by using a random string.
 func GeneratePKCE() (codeVerifier, challenge string) {
-	codeVerifier = GenerateID()[:32]
+	code := GenerateKey()
+	codeVerifier = base64.RawURLEncoding.EncodeToString(code[:])
 	sum := sha256.Sum256([]byte(codeVerifier))
 	challenge = base64.RawURLEncoding.EncodeToString(sum[:])
 	return
