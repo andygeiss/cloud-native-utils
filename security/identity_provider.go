5afcc8cc8cc0228cf50c8345e486eac9ece40f9b
andygeiss <4ndygeiss@gmail.com>
2025-09-26 10:28:16 +0200

    refactor: add middleware

--- security/identity_provider.go
+++ security/identity_provider.go
@@ -19,17 +19,6 @@ type identityProvider struct {
 	stateCodeVerifiers resource.Access[string, string]
 }
 
-type ContextKey string
-
-const (
-	ContextSessionID ContextKey = "session_id"
-	ContextEmail     ContextKey = "email"
-	ContextIssuer    ContextKey = "issuer"
-	ContextName      ContextKey = "name"
-	ContextSubject   ContextKey = "subject"
-	ContextVerified  ContextKey = "verified"
-)
-
 // IdentityTokenClaims represents the claims of an identity token.
 type IdentityTokenClaims struct {
 	Email    string `json:"email"`
@@ -145,41 +134,6 @@ func (a *identityProvider) Logout(sessions *ServerSessions) http.HandlerFunc {
 
 		// Redirect the user to the logout URL.
 		http.Redirect(w, r, os.Getenv("REDIRECT_URL"), http.StatusFound)
-	}
-}
-
-// WithAuth adds authentication information to the context.
-func (a *identityProvider) WithAuth(sessions *ServerSessions, next http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		// Extract the current context.
-		ctx := r.Context()
-
-		// Retrieve the session ID from the request URL.
-		sessionId := r.URL.Query().Get("session_id")
-
-		// Define the claims
-		var email, issuer, name, subject string
-
-		if sessionId != "" {
-			// Retrieve the session by using the session ID.
-			if session, ok := sessions.Read(sessionId); ok {
-				claims, _ := session.Data.(IdentityTokenClaims)
-				email = claims.Email
-				issuer = claims.Issuer
-				name = claims.Name
-				subject = claims.Subject
-			}
-		}
-
-		// Add the authentication informations.
-		ctx = context.WithValue(ctx, ContextEmail, email)
-		ctx = context.WithValue(ctx, ContextIssuer, issuer)
-		ctx = context.WithValue(ctx, ContextName, name)
-		ctx = context.WithValue(ctx, ContextSubject, subject)
-		ctx = context.WithValue(ctx, ContextSessionID, sessionId)
-
-		// Call the next http handler with context.
-		next.ServeHTTP(w, r.WithContext(ctx))
 	}
 }
 
---
+++ security/middleware.go
@@ -0,0 +1,52 @@
+package security
+
+import (
+	"context"
+	"net/http"
+)
+
+type ContextKey string
+
+const (
+	ContextSessionID ContextKey = "session_id"
+	ContextEmail     ContextKey = "email"
+	ContextIssuer    ContextKey = "issuer"
+	ContextName      ContextKey = "name"
+	ContextSubject   ContextKey = "subject"
+	ContextVerified  ContextKey = "verified"
+)
+
+// WithAuth adds authentication information to the context.
+func (a *identityProvider) WithAuth(sessions *ServerSessions, next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Extract the current context.
+		ctx := r.Context()
+
+		// Retrieve the session ID from the request URL.
+		sessionId := r.URL.Query().Get("session_id")
+
+		// Define the claims
+		var email, issuer, name, subject string
+
+		if sessionId != "" {
+			// Retrieve the session by using the session ID.
+			if session, ok := sessions.Read(sessionId); ok {
+				claims, _ := session.Data.(IdentityTokenClaims)
+				email = claims.Email
+				issuer = claims.Issuer
+				name = claims.Name
+				subject = claims.Subject
+			}
+		}
+
+		// Add the authentication informations.
+		ctx = context.WithValue(ctx, ContextEmail, email)
+		ctx = context.WithValue(ctx, ContextIssuer, issuer)
+		ctx = context.WithValue(ctx, ContextName, name)
+		ctx = context.WithValue(ctx, ContextSubject, subject)
+		ctx = context.WithValue(ctx, ContextSessionID, sessionId)
+
+		// Call the next http handler with context.
+		next.ServeHTTP(w, r.WithContext(ctx))
+	}
+}
