ec740563db8f21137c66ad310e354c1b2fe748b6
Andy <4ndygeiss@gmail.com>
2025-11-21 12:13:13 +0100

    test: add plugin tests

--- extensibility/load_plugin_test.go
+++ extensibility/load_plugin_test.go
@@ -11,9 +11,19 @@ type TestPort interface {
 	FindByID(id string) (name string, err error)
 }
 
-func TestLoadPlugin(t *testing.T) {
+func TestLoadPlugin_OK(t *testing.T) {
 	adapter, err := extensibility.LoadPlugin[TestPort]("testdata/adapter.so", "Adapter")
 	name, err := adapter.FindByID("1")
 	assert.That(t, "err must be nil", err, nil)
 	assert.That(t, "adapter.FindByID must return 'Andy'", name, "Andy")
+}
+
+func TestLoadPlugin_Error_Open(t *testing.T) {
+	_, err := extensibility.LoadPlugin[TestPort]("testdata/adapter2.so", "Adapter")
+	assert.That(t, "err must not be nil", err != nil, true)
+}
+
+func TestLoadPlugin_Error_Load(t *testing.T) {
+	_, err := extensibility.LoadPlugin[TestPort]("testdata/adapter.so", "Adapter2")
+	assert.That(t, "err must not be nil", err != nil, true)
 }
