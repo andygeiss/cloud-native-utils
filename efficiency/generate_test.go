031b7c5bbab0a2860b23bcb7086126af77dc3e10
Andy <4ndygeiss@gmail.com>
2025-11-23 10:16:36 +0100

    test: remove duplicate

--- efficiency/generate_test.go
+++ efficiency/generate_test.go
@@ -7,17 +7,7 @@ import (
 	"github.com/andygeiss/cloud-native-utils/efficiency"
 )
 
-func TestGenerate_Empty(t *testing.T) {
-	in := []int{}
-	ch := efficiency.Generate[int](in...)
-	out := make([]int, 0)
-	for val := range ch {
-		out = append(out, val)
-	}
-	assert.That(t, "out slice len must be correct", len(out), 0)
-}
-
-func TestGenerate_Three_Int(t *testing.T) {
+func TestGenerate_OK(t *testing.T) {
 	in := []int{1, 2, 3}
 	ch := efficiency.Generate[int](in...)
 	out := make([]int, 0)
