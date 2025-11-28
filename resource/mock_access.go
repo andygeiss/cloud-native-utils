cd979ecf8b77fef1faa4c5142f7d8d16ffe30ebe
Andy <4ndygeiss@gmail.com>
2025-11-28 14:19:55 +0100

    refactor: clean up mock

--- resource/mock_access.go
+++ resource/mock_access.go
@@ -1,68 +1,73 @@
 package resource
 
-// MockAccess is a mock implementation of Access[K, V].
+// mockAccess is a mock implementation of Access[K, V].
 // Each method has a corresponding mock function pointer (...Ptr).
 // Use the builder-pattern to set the mock function pointers for each method.
 // This allows for more flexible and readable test cases.
-type MockAccess[K, V any] struct {
-	CreateFn  func(key K, value V) error
-	ReadFn    func(key K) (*V, error)
-	ReadAllFn func() ([]V, error)
-	UpdateFn  func(key K, value V) error
-	DeleteFn  func(key K) error
+type mockAccess[K, V any] struct {
+	createFn  func(key K, value V) error
+	readFn    func(key K) (*V, error)
+	readAllFn func() ([]V, error)
+	updateFn  func(key K, value V) error
+	deleteFn  func(key K) error
 }
 
 // NewMockAccess creates a new instance of MockAccess[K, V].
-func NewMockAccess[K, V any]() *MockAccess[K, V] {
-	return &MockAccess[K, V]{}
+func NewMockAccess[K, V any]() *mockAccess[K, V] {
+	return &mockAccess[K, V]{}
 }
 
 // Create creates a new resource with the given key and value.
-func (a *MockAccess[K, V]) Create(key K, value V) error {
-	return a.CreateFn(key, value)
+func (a *mockAccess[K, V]) Create(key K, value V) error {
+	return a.createFn(key, value)
 }
 
 // Read reads a resource with the given key.
-func (a *MockAccess[K, V]) Read(key K) (value *V, err error) {
-	return a.ReadFn(key)
+func (a *mockAccess[K, V]) Read(key K) (value *V, err error) {
+	return a.readFn(key)
 }
 
 // ReadAll reads all resources.
-func (a *MockAccess[K, V]) ReadAll() (values []V, err error) {
-	return a.ReadAllFn()
+func (a *mockAccess[K, V]) ReadAll() (values []V, err error) {
+	return a.readAllFn()
 }
 
 // Update updates a resource with the given key and value.
-func (a *MockAccess[K, V]) Update(key K, value V) (err error) {
-	return a.UpdateFn(key, value)
+func (a *mockAccess[K, V]) Update(key K, value V) (err error) {
+	return a.updateFn(key, value)
 }
 
 // Delete deletes a resource with the given key.
-func (a *MockAccess[K, V]) Delete(key K) (err error) {
-	return a.DeleteFn(key)
+func (a *mockAccess[K, V]) Delete(key K) (err error) {
+	return a.deleteFn(key)
 }
 
-// WithCreatePtr sets the mock function pointer for creating a resource.
-func (a *MockAccess[K, V]) WithCreatePtr(fn func(key K, value V) error) {
-	a.CreateFn = fn
+// WithCreateFn sets the mock function pointer for creating a resource.
+func (a *mockAccess[K, V]) WithCreateFn(fn func(key K, value V) error) *mockAccess[K, V] {
+	a.createFn = fn
+	return a
 }
 
-// WithReadPtr sets the mock function pointer for reading a resource.
-func (a *MockAccess[K, V]) WithReadPtr(fn func(key K) (*V, error)) {
-	a.ReadFn = fn
+// WithReadFn sets the mock function pointer for reading a resource.
+func (a *mockAccess[K, V]) WithReadFn(fn func(key K) (*V, error)) *mockAccess[K, V] {
+	a.readFn = fn
+	return a
 }
 
-// WithReadAllPtr sets the mock function pointer for reading all resources.
-func (a *MockAccess[K, V]) WithReadAllPtr(fn func() ([]V, error)) {
-	a.ReadAllFn = fn
+// WithReadAllFn sets the mock function pointer for reading all resources.
+func (a *mockAccess[K, V]) WithReadAllFn(fn func() ([]V, error)) *mockAccess[K, V] {
+	a.readAllFn = fn
+	return a
 }
 
-// WithUpdatePtr sets the mock function pointer for updating a resource.
-func (a *MockAccess[K, V]) WithUpdatePtr(fn func(key K, value V) error) {
-	a.UpdateFn = fn
+// WithUpdateFn sets the mock function pointer for updating a resource.
+func (a *mockAccess[K, V]) WithUpdateFn(fn func(key K, value V) error) *mockAccess[K, V] {
+	a.updateFn = fn
+	return a
 }
 
-// WithDeletePtr sets the mock function pointer for deleting a resource.
-func (a *MockAccess[K, V]) WithDeletePtr(fn func(key K) error) {
-	a.DeleteFn = fn
+// WithDeleteFn sets the mock function pointer for deleting a resource.
+func (a *mockAccess[K, V]) WithDeleteFn(fn func(key K) error) *mockAccess[K, V] {
+	a.deleteFn = fn
+	return a
 }
--- resource/mock_access_test.go
+++ resource/mock_access_test.go
@@ -9,37 +9,37 @@ import (
 )
 
 func TestMockAccessCreate_OK(t *testing.T) {
-	a := resource.NewMockAccess[int, int]()
 	isCalled := false
-	a.CreateFn = func(key int, value int) error {
-		isCalled = true
-		return nil
-	}
+	a := resource.NewMockAccess[int, int]().
+		WithCreateFn(func(key int, value int) error {
+			isCalled = true
+			return nil
+		})
 	err := a.Create(0, 42)
 	assert.That(t, "err must be nil", err, nil)
 	assert.That(t, "create must be called", isCalled, true)
 }
 
 func TestMockAccessCreate_Error(t *testing.T) {
-	a := resource.NewMockAccess[int, int]()
 	isCalled := false
-	a.CreateFn = func(key int, value int) error {
-		isCalled = true
-		return errors.New("error")
-	}
+	a := resource.NewMockAccess[int, int]().
+		WithCreateFn(func(key int, value int) error {
+			isCalled = true
+			return errors.New("error")
+		})
 	err := a.Create(0, 42)
 	assert.That(t, "err must be correct", err.Error(), "error")
 	assert.That(t, "create must be called", isCalled, true)
 }
 
 func TestMockAccessRead_OK(t *testing.T) {
-	a := resource.NewMockAccess[int, int]()
 	isCalled := false
-	a.ReadFn = func(key int) (*int, error) {
-		isCalled = true
-		value := 42
-		return &value, nil
-	}
+	a := resource.NewMockAccess[int, int]().
+		WithReadFn(func(key int) (*int, error) {
+			isCalled = true
+			value := 42
+			return &value, nil
+		})
 	value, err := a.Read(0)
 	assert.That(t, "err must be nil", err, nil)
 	assert.That(t, "read must be called", isCalled, true)
@@ -47,12 +47,39 @@ func TestMockAccessRead_OK(t *testing.T) {
 }
 
 func TestMockAccessRead_Error(t *testing.T) {
-	a := resource.NewMockAccess[int, int]()
 	isCalled := false
-	a.ReadFn = func(key int) (*int, error) {
-		isCalled = true
-		return nil, errors.New("error")
-	}
+	a := resource.NewMockAccess[int, int]().
+		WithReadFn(func(key int) (*int, error) {
+			isCalled = true
+			return nil, errors.New("error")
+		})
+	value, err := a.Read(0)
+	assert.That(t, "err must be correct", err.Error(), "error")
+	assert.That(t, "read must be called", isCalled, true)
+	assert.That(t, "value must be nil", value == nil, true)
+}
+
+func TestMockAccessReadAll_OK(t *testing.T) {
+	isCalled := false
+	a := resource.NewMockAccess[int, int]().
+		WithReadAllFn(func() ([]int, error) {
+			isCalled = true
+			value := 42
+			return []int{value}, nil
+		})
+	value, err := a.Read(0)
+	assert.That(t, "err must be nil", err, nil)
+	assert.That(t, "read must be called", isCalled, true)
+	assert.That(t, "value must be correct", *value, 42)
+}
+
+func TestMockAccessReadAll_Error(t *testing.T) {
+	isCalled := false
+	a := resource.NewMockAccess[int, int]().
+		WithReadAllFn(func() ([]int, error) {
+			isCalled = true
+			return nil, errors.New("error")
+		})
 	value, err := a.Read(0)
 	assert.That(t, "err must be correct", err.Error(), "error")
 	assert.That(t, "read must be called", isCalled, true)
@@ -60,14 +87,14 @@ func TestMockAccessRead_Error(t *testing.T) {
 }
 
 func TestMockAccessUpdate_OK(t *testing.T) {
-	a := resource.NewMockAccess[int, int]()
 	isCalled := false
 	val := 0
-	a.UpdateFn = func(key int, value int) error {
-		isCalled = true
-		val = value
-		return nil
-	}
+	a := resource.NewMockAccess[int, int]().
+		WithUpdateFn(func(key int, value int) error {
+			isCalled = true
+			val = value
+			return nil
+		})
 	err := a.Update(0, 1)
 	assert.That(t, "err must be nil", err, nil)
 	assert.That(t, "update must be called", isCalled, true)
@@ -75,36 +102,36 @@ func TestMockAccessUpdate_OK(t *testing.T) {
 }
 
 func TestMockAccessUpdate_Error(t *testing.T) {
-	a := resource.NewMockAccess[int, int]()
 	isCalled := false
-	a.UpdateFn = func(key int, value int) error {
-		isCalled = true
-		return errors.New("error")
-	}
+	a := resource.NewMockAccess[int, int]().
+		WithUpdateFn(func(key int, value int) error {
+			isCalled = true
+			return errors.New("error")
+		})
 	err := a.Update(0, 42)
 	assert.That(t, "err must be correct", err.Error(), "error")
 	assert.That(t, "update must be called", isCalled, true)
 }
 
 func TestMockAccessDelete_OK(t *testing.T) {
-	a := resource.NewMockAccess[int, int]()
 	isCalled := false
-	a.DeleteFn = func(key int) error {
-		isCalled = true
-		return nil
-	}
+	a := resource.NewMockAccess[int, int]().
+		WithDeleteFn(func(key int) error {
+			isCalled = true
+			return nil
+		})
 	err := a.Delete(0)
 	assert.That(t, "err must be nil", err, nil)
 	assert.That(t, "delete must be called", isCalled, true)
 }
 
 func TestMockAccessDelete_Error(t *testing.T) {
-	a := resource.NewMockAccess[int, int]()
 	isCalled := false
-	a.DeleteFn = func(key int) error {
-		isCalled = true
-		return errors.New("error")
-	}
+	a := resource.NewMockAccess[int, int]().
+		WithDeleteFn(func(key int) error {
+			isCalled = true
+			return errors.New("error")
+		})
 	err := a.Delete(0)
 	assert.That(t, "err must be correct", err.Error(), "error")
 	assert.That(t, "delete must be called", isCalled, true)
