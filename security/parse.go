6af1fcb51d153fc6e410558f1c9cb5f37746d816
Andy <4ndygeiss@gmail.com>
2025-12-17 09:34:47 +0100

    feat: add parse bool and string

--- security/parse.go
+++ security/parse.go
@@ -6,6 +6,17 @@ import (
 	"time"
 )
 
+// ParseBoolOrDefault parses the value of the environment variable with the given key as a boolean.
+// If the value is not set or cannot be parsed, the default boolean is returned.
+func ParseBoolOrDefault(key string, def bool) bool {
+	if value := os.Getenv(key); value != "" {
+		if duration, err := strconv.ParseBool(value); err == nil {
+			return duration
+		}
+	}
+	return def
+}
+
 // ParseDurationOrDefault parses the value of the environment variable with the given key as a duration.
 // If the value is not set or cannot be parsed, the default duration is returned.
 func ParseDurationOrDefault(key string, def time.Duration) time.Duration {
@@ -24,6 +35,15 @@ func ParseIntOrDefault(key string, def int) int {
 		if duration, err := strconv.Atoi(value); err == nil {
 			return duration
 		}
+	}
+	return def
+}
+
+// ParseStringOrDefault parses the value of the environment variable with the given key as a string.
+// If the value is not set or cannot be parsed, the default string is returned.
+func ParseStringOrDefault(key string, def string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
 	}
 	return def
 }
--- security/parse_test.go
+++ security/parse_test.go
@@ -9,6 +9,17 @@ import (
 	"github.com/andygeiss/cloud-native-utils/security"
 )
 
+func TestParseBoolOrDefault_Env_Not_Set_Uses_Default(t *testing.T) {
+	value := security.ParseBoolOrDefault("CLIENT_ENABLED", true)
+	assert.That(t, "value must be true", value, true)
+}
+
+func TestParseBoolOrDefault_Env_Set(t *testing.T) {
+	os.Setenv("CLIENT_ENABLED", "false")
+	value := security.ParseBoolOrDefault("CLIENT_ENABLED", true)
+	assert.That(t, "value must be false", value, false)
+}
+
 func TestParseDurationOrDefault_Env_Not_Set_Uses_Default(t *testing.T) {
 	d := security.ParseDurationOrDefault("CLIENT_TIMEOUT", 5*time.Second)
 	assert.That(t, "duration must be 5 seconds", d, 5*time.Second)
@@ -29,4 +40,15 @@ func TestParseIntOrDefault_Env_Set(t *testing.T) {
 	os.Setenv("CLIENT_COUNT", "3")
 	value := security.ParseIntOrDefault("CLIENT_COUNT", 5)
 	assert.That(t, "value must be 3", value, 3)
+}
+
+func TestParseStringOrDefault_Env_Not_Set_Uses_Default(t *testing.T) {
+	value := security.ParseStringOrDefault("CLIENT_NAME", "default")
+	assert.That(t, "value must be default", value, "default")
+}
+
+func TestParseStringOrDefault_Env_Set(t *testing.T) {
+	os.Setenv("CLIENT_NAME", "test")
+	value := security.ParseStringOrDefault("CLIENT_NAME", "default")
+	assert.That(t, "value must be test", value, "test")
 }
